038787a39f4813db16f7e3816cd16182fdab5ce2
wilitp <guilledeipola@gmail.com>
2020-05-08 18:40:33 -0300

    Cambiada la carta personal en la sección de estudio

--- src/components/Estudio/Estudio.js
+++ src/components/Estudio/Estudio.js
@@ -3,7 +3,7 @@ import classes from './Estudio.module.scss';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Estudio = () => {
   const data = useStaticQuery(graphql`
@@ -33,12 +33,16 @@ const Estudio = () => {
 
             <div className={classes.icons}>
               <a href="https://www.linkedin.com/in/viviana-tobar-416848124/" target="blank">
-                <Icon icon={faLinkedinIn} />
+                <Icon icon={faLinkedinIn} size="lg" />
               </a>
               <a href="https://www.facebook.com/viviana.tobar.35" target="blank">
-                <Icon icon={faFacebookF}/>
+                <Icon icon={faFacebookF} size="lg" />
+              </a>
+              <a href="https://www.instagram.com/viviliztobar/?hl=es">
+                <Icon icon={faInstagram} size="lg" />
               </a>
             </div>
+
           </div>
         </div>
       </div>
